914372c5ab8cb53e38fe18027943f9d6320dc24f
HELLO-stack-ai <79909814+HELLO-stack-ai@users.noreply.github.com>
2021-03-06 22:55:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var inputs=[];
+var i=0;
+
+function joint(){
+    for(var i = 1 ; i <= 6 ; i++)
+    inputs.push(document.getElementById("name_of_the_student_"+i).divstart.value);
+    document.getElementById("divstart").innerHTML = "div1".join("div2")
+}
